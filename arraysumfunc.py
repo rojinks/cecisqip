50c86bae433389076c7d983f055198baba4c854f
Govind Kartha <44610432+govkartha@users.noreply.github.com>
2020-10-01 23:28:31 +0530

    arraysum

--- arraysumfunc.py
+++ arraysumfunc.py
@@ -1,26 +1,3 @@
-import os
-import sys
-
-#
-# Complete the simpleArraySum function below.
-#
-def simpleArraySum(ar):
-    #
-    sum = 0
-    for i in range(0,len(ar)):
-      sum += ar[i]
-    return sum
-    #
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = simpleArraySum(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+number_of_elements = int(input())
+array = map(int, input().split())
+print(sum(array))
